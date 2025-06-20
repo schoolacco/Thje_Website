5bb7d951b3d24c66ca47edc79784cda097534188
schoolacco <anthony.revel510@education.nsw.gov.au>
2025-06-20 11:09:02 +1000

    I honestly have no idea

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from app import app
+from app import app, db
+from flask_login import current_user, login_user, logout_user
+import sqlalchemy as sa
 from flask import render_template, redirect, flash, url_for
 from app.login import LoginForm
+from app.models import User
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
@@ -22,8 +25,19 @@ def test():
 @app.route('/login')
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember me={}'.format(form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
