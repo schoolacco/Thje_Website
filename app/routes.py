71b8e9dedc98d0b5b15a29eb9d31b28da1a7dc59
schoolacco <anthony.revel510@education.nsw.gov.au>
2025-06-26 10:08:49 +1000

    Still confused as always.

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from app import app, db
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
-from flask import render_template, redirect, flash, url_for
-from app.login import LoginForm
+from flask import render_template, redirect, flash, url_for, request
+from urllib.parse import urlsplit
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
+@login_required
 def index():
     return render_template('index.html')
 @app.route('/about')
@@ -35,9 +37,25 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', form=form)
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+@app.route('register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
